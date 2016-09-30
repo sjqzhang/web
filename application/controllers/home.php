71c81034a81248fdfa608a41e68bbc19bd57dfa3
sjqzhang <s_jqzhang@163.com>
2016-09-30 14:44:34 +0800

    Update home.php
    support ldap and user && password

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -16,86 +16,92 @@ class Home extends Base_Controller
 
     public function login()
     {
-		/*
-        if (IS_POST) {
-            $_username = $this->input->post("user_name");
-            $_password = $this->input->post("password");
-            $this->load->model("sys_user_model");
-			
-            $this->load->library('ldap');
-			$_username=str_replace(LDAP_EMAIL, '', $_username);
-            $ldap_res = $this->ldap->bind($_username,$_password);
-            $_admin = $this->sys_user_model->getEntity(array("user_name" => $_username));
-            if($ldap_res == 1){
-                     if(!empty($_admin)){
-                            if ($_admin['flag_valid'] == 1) {
-                                unset($_admin['password']);
 
-                                 MAUTH::after_login($_admin,true);
-			                	$this->session->set_userdata(array("admin" => $_admin));
-			               		$ot = array('code' => 0,
-			                    	'message' => '登录成功!');
-				            } else {
-				                $ot = array('code' => 1001,
-				                    'message' => '账号已停用!');
-				            }
-                     }else{ //添加用户
-
-                            $_data = array(
-                                    'user_name'=>$_username,
-                                    'truename'=>$_username,
-                                    'password'=>md5($_password.time()),
-                                    'email'=> $_username.LDAP_EMAIL,
-                                    'createdate'=>date('Y-m-d H:i:s'),
-                                    'sys_group_id'=>'2',//普通用户组 这个>等用户组表调整以后修改
-                                    'flag_valid'=>1
-                            );
-                            $this->sys_user_model->addEntity($_data);
-                            //获取用户 用session保存登录跟上面保持一致
-                            $_admin = $this->sys_user_model->getEntity(array("user_name" => $_username));
-                            $this->session->set_userdata(array("admin" => $_admin));
-                             unset($_admin['password']);
-                            MAuth::after_login($_admin,true);
-					        $ot = array('code' => 0,
-					            'message' => '登录成功!');
-                     }
-            }else{
-                    $ot = array('code' => 1002,
-        				'message' => '账号或密码错误!');
-            }
-
-            echo json_encode($ot);
-
-
-        } else {
-		 $this->load->view('login');
-		}
-			*/
-			
-        
-		if (IS_POST) {
-            $_username = $this->input->post("user_name");
-			$_password = $this->input->post("password");
-			$_admin = $this->sys_user_model->getEntity(array("user_name" => $_username,'password'=>md5($_password)));
-            if (!empty($_admin)) {
-                if ($_admin['flag_valid'] == 1) {
-                    $this->session->set_userdata(array("admin" => $_admin));
-					MAUTH::after_login($_admin,true);
-                    $ot = array('code' => 0,
-                        'message' => '登录成功!');
-                } else {
-                    $ot = array('code' => 1001,
-                        'message' => '账号已停用!');
-                }
-            } else {
-                $ot = array('code' => 1002,
-                    'message' => '账号或密码错误!');
-            }
-            echo json_encode($ot);
-        } else {
-            $this->load->view('login');
-        }
+		if($this->config->config['use_ldap']){
 		
+		
+			if (IS_POST) {
+				$_username = $this->input->post("user_name");
+				$_password = $this->input->post("password");
+				$this->load->model("sys_user_model");
+				
+				$this->load->library('ldap');
+				$_username=str_replace(LDAP_EMAIL, '', $_username);
+				$ldap_res = $this->ldap->bind($_username,$_password);
+				$_admin = $this->sys_user_model->getEntity(array("user_name" => $_username));
+				if($ldap_res == 1){
+						 if(!empty($_admin)){
+								if ($_admin['flag_valid'] == 1) {
+									unset($_admin['password']);
+
+									 MAUTH::after_login($_admin,true);
+									$this->session->set_userdata(array("admin" => $_admin));
+									$ot = array('code' => 0,
+										'message' => '登录成功!');
+								} else {
+									$ot = array('code' => 1001,
+										'message' => '账号已停用!');
+								}
+						 }else{ //添加用户
+
+								$_data = array(
+										'user_name'=>$_username,
+										'truename'=>$_username,
+										'password'=>md5($_password.time()),
+										'email'=> $_username.LDAP_EMAIL,
+										'createdate'=>date('Y-m-d H:i:s'),
+										'sys_group_id'=>'2',//普通用户组 这个>等用户组表调整以后修改
+										'flag_valid'=>1
+								);
+								$this->sys_user_model->addEntity($_data);
+								//获取用户 用session保存登录跟上面保持一致
+								$_admin = $this->sys_user_model->getEntity(array("user_name" => $_username));
+								$this->session->set_userdata(array("admin" => $_admin));
+								 unset($_admin['password']);
+								MAuth::after_login($_admin,true);
+								$ot = array('code' => 0,
+									'message' => '登录成功!');
+						 }
+				}else{
+						$ot = array('code' => 1002,
+							'message' => '账号或密码错误!');
+				}
+
+				echo json_encode($ot);
+
+
+			} else {
+			 $this->load->view('login');
+			}
+
+		} else {
+			
+		
+        
+			if (IS_POST) {
+				$_username = $this->input->post("user_name");
+				$_password = $this->input->post("password");
+				$_admin = $this->sys_user_model->getEntity(array("user_name" => $_username,'password'=>md5($_password)));
+				if (!empty($_admin)) {
+					if ($_admin['flag_valid'] == 1) {
+						$this->session->set_userdata(array("admin" => $_admin));
+						MAUTH::after_login($_admin,true);
+						$ot = array('code' => 0,
+							'message' => '登录成功!');
+					} else {
+						$ot = array('code' => 1001,
+							'message' => '账号已停用!');
+					}
+				} else {
+					$ot = array('code' => 1002,
+						'message' => '账号或密码错误!');
+				}
+				echo json_encode($ot);
+			} else {
+				$this->load->view('login');
+			}
+			
+		}
 
     }
 
