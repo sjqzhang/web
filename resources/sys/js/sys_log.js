bcf2281b9dc20a6a9611fd247b09786be57976cb
sjqzhang <s_jqzhang@163.com>
2016-11-10 10:27:29 +0800

    remove sql code column

--- resources/sys/js/sys_log.js
+++ resources/sys/js/sys_log.js
@@ -21,15 +21,15 @@ SysLog.prototype={
     
             var cols = [
                 
-	{title: '编号', name : 'id', width : 11, sortable : false, align: 'left'},
-		{title: '路径', name : 'url', width : 11, sortable : false, align: 'left',renderer:this.rendertitle},
-		{title: '参数', name : 'params', width : 11, sortable : false, align: 'left',renderer:this.rendertitle},
-		{title: '编码', name : 'error_code', width : 11, sortable : false, align: 'left'},
-		{title: '信息', name : 'error_message', width : 11, sortable : false, align: 'left',renderer:this.rendertitle},
-		{title: '用户IP', name : 'ip', width : 11, sortable : false, align: 'left'},
-		{title: '用户编号', name : 'admin_id', width : 11, sortable : false, align: 'left'},
-		{title: '时间', name : 'time', width : 11, sortable : false, align: 'left',renderer:this.rendertime},
-		{title: '执行的sql', name : 'query', width : 11, sortable : false, align: 'left',renderer:this.rendertitle}
+	{title: '编号', name : 'id', width : 10, sortable : false, align: 'left'},
+		{title: '路径', name : 'url', width : 120, sortable : false, align: 'left',renderer:this.rendertitle},
+		{title: '参数', name : 'params', width : 500, sortable : false, align: 'left',renderer:this.rendertitle},
+		//{title: '编码', name : 'error_code', width : 11, sortable : false, align: 'left'},
+		{title: '信息', name : 'error_message', width : 100, sortable : false, align: 'left',renderer:this.rendertitle},
+		{title: '用户IP', name : 'ip', width : 100, sortable : false, align: 'left'},
+		{title: '用户编号', name : 'admin_id', width : 50, sortable : false, align: 'left'},
+		{title: '时间', name : 'time', width : 100, sortable : false, align: 'left',renderer:this.rendertime}
+		//,{title: '执行的sql', name : 'query', width : 11, sortable : false, align: 'left',renderer:this.rendertitle}
 ];
             
 
@@ -37,7 +37,8 @@ SysLog.prototype={
 //AJAX示例
 var mmg =  $(this.gridId).mmGrid({
     cols: cols,
-	height:'550px',
+    fullWidthRows:true,
+	height:window.screen.height -450,
     url: self.searchUrl,
     method: 'post',
     remoteSort:true ,
@@ -52,9 +53,9 @@ var mmg =  $(this.gridId).mmGrid({
     plugins : [
         $(self.pageBarId).mmPaginator()
     ],
-    checkCol:true,
-    indexCol:true,
-    multiSelect:true,
+  //  checkCol:true,
+   // indexCol:true,
+  //  multiSelect:true,
 		nowrap:true
 
 });
